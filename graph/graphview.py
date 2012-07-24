aa4b34a91421228c330aaa861bab104de64941bc
evertang <tj@gameface.me>
2012-07-24 08:45:49 -0400

    fix wrong import

--- graph/graphview.py
+++ graph/graphview.py
@@ -3,9 +3,6 @@ from django.template import loader
 from django.template.context import RequestContext
 from django.views.decorators.http import require_http_methods
 
-from onechart.models import *
-from onechart.managers import *
-
 
 @require_http_methods(["GET", "POST", "HEAD"])
 def handler(req, precon_id=None):
