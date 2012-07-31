4c2bc0c99633ef19ea7f56f6d78f64f9071d5da9
evertang <tj@gameface.me>
2012-07-31 19:52:59 -0400

    restore the old page

--- graph/graphview.py
+++ graph/graphview.py
@@ -6,8 +6,10 @@ from django.views.decorators.http import require_http_methods
 
 @require_http_methods(["GET", "POST", "HEAD"])
 def handler(req, precon_id=None):
-    template = loader.get_template('graph.html')
-    template = loader.get_template('ext.html')
+    if req.REQUEST.get('old'):
+        template = loader.get_template('graph.html')
+    else:
+        template = loader.get_template('ext.html')
     #ctx = gf_template.get_context(req, {})
     ctx = RequestContext(req, {})
     
