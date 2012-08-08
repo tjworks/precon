84ecc4d9133987c580dd92da1e0dd9007ab98c74
joeliao <lxjiulxjiu@gmail.com>
2012-08-08 10:07:55 -0700

    try to bugfix link create

--- static/ext/js/main.js
+++ static/ext/js/main.js
@@ -837,6 +837,7 @@ function linkCreate() {
 															var	node2=null;
 															nodearray.forEach(function(anode){
 																if (anode.getLabel().toLowerCase()==Ext.getCmp('linkname1_c').getValue().toLowerCase()) {
+																	console.log(anode.getLabel()+"<===>"+anode.getId());
 																	node1=new precon.Node({"label":anode.getLabel(), "_id":anode.getId()});
 																}
 																if (anode.getLabel().toLowerCase()==Ext.getCmp('linkname2_c').getValue().toLowerCase()) {
