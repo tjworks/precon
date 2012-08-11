120e9d1080171db43259b541d42c7fdc9245cf78
evertang <tj@gameface.me>
2012-08-10 22:48:49 -0400

    fix gen_index findAndModify problem

--- mongojs/js/gen_index.js
+++ mongojs/js/gen_index.js
@@ -1,8 +1,10 @@
 
-function(){
-
+function(cutoff){
+	cutoff = cutoff || '2012-08-01 00:00:00'
+	var query = {create_tm:{$gt:cutoff}}
     chop = function(seq){
-        return seq.split(/[\s-\.$"'\(\)_]/)
+        if(!seq) return seq
+		return seq.split(/[\s-\.$"'\(\)_]/)
     }          
     insertKeywords=function(keywords, obj){
         for (var k in keywords) {
@@ -11,45 +13,56 @@ function(){
                kwd = kwd.toLowerCase()                               
                updates = {}
                updates['names.'+ obj._id]  =  obj.name                               
-               db.indices.findAndModify({
-                     query: {_id: kwd},                                     
-                     update: {$set: updates, $addToSet:{ids :obj._id}},
-                     new: true,
-                     upsert:true
-                });                               
+               db.indices.update(
+                     {_id: kwd},                                     
+                     {$set: updates, $addToSet:{ids :obj._id}},                     
+                     true
+                );                               
            }    
     }      
-    db.publication.find().forEach( function(obj){                           
+    count = 0
+    
+    db.publication.find(query).forEach( function(obj){                           
            var keywords = [];
            keywords.push(obj.pubmed_id);
            keywords = keywords.concat( chop(obj.name) );
            keywords.push(obj.name)
            obj.name=obj.pubmed_id+", "+ obj.name                           
-           insertKeywords(keywords,obj);                    
-    });                    
-    db.entity.find().forEach( function(obj){                           
+           insertKeywords(keywords,obj);
+           count++;
+    });    
+    log("Processed "+ count+ " publications")
+    
+    count=0
+    db.entity.find(query).forEach( function(obj){                           
            var keywords = []
            keywords.push(obj.symbol)
            keywords = keywords.concat( chop(obj.name) )
            keywords.push(obj.name)
            if(obj.name)
                obj.name=obj.symbol +", "+ obj.name
-           insertKeywords(keywords,obj);                    
+           insertKeywords(keywords,obj);
+           count++
     });
-    
-    db.network.find().forEach( function(obj){                           
+    log("Processing "+count+" entities")
+    count=0
+    db.network.find(query).forEach( function(obj){                           
            if(!obj.name) return;
            var keywords = chop(obj.name);
            keywords.push(obj.name)                                                                                 
-           insertKeywords(keywords,obj);                    
+           insertKeywords(keywords,obj);
+           count++;
     });
-    db.people.find().forEach( function(obj){                           
+    log("Processed network "+ count)
+    count = 0
+    db.people.find(query).forEach( function(obj){                           
            var keywords = [obj.first, obj.last] ;                                                      
            obj.name=obj.first +" "+ obj.last
            keywords.push(obj.name)
-           insertKeywords(keywords,obj);                    
+           insertKeywords(keywords,obj);
+           count++;
     });
-                                    
+    log("Processed people "+ count)
 }
 
 
